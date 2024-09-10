683df74f75045b2e88a2224961c225d3cfdd651c
themagesh <themagesh.v@gmail.com>
2024-09-10 13:29:20 +0530

    satock id auto genrat

--- client/forms.py
+++ client/forms.py
@@ -4,10 +4,10 @@ class StockForm(forms.Form):
 
     stockCode = forms.CharField(max_length=100)
     companyName = forms.CharField(max_length=30)
-    industry=forms.CharField(max_length=30)
-    sector=forms.CharField(max_length=20)
+    industry=forms.CharField(max_length=40)
+    sector=forms.CharField(max_length=30)
     website=forms.CharField(max_length=100)
-    about= forms.CharField(max_length=1000)
+    about= forms.CharField(max_length=3000)
     singleLine=forms.CharField(max_length=300)
     upTrend=forms.CharField(max_length=300)
     downTrend=forms.CharField(max_length=300)
